03cc44ee3a2c6fe2d227f8a63c1661f2ea4158b6
osmanforhad <osmanforhad93@gmail.com>
2022-02-27 17:36:06 +0600

    working with date formating techniq by using luxon package

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
@@ -16,6 +17,11 @@ var BookInstanceSchema = new Schema({
 //__Virtual for bookinstance's URL__//
 BookInstanceSchema.virtual("url").get(function () {
   return "/catalog/bookinstance/" + this._id;
+});
+
+//__Date time Format__//
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //__Export Model__//
