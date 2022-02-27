0bf8ae258f6b79e5dc5e70890dad663d7a397aac
osmanforhad <osmanforhad93@gmail.com>
2022-02-27 17:11:25 +0600

    working with home page or catalog page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,38 @@
 var Book = require("../models/book");
+var Author = require("../models/author");
+var Genre = require("../models/genre");
+var BookInstance = require("../models/bookinstance");
 
+var async = require("async");
+
+//__Home__//
 exports.index = function (request, response) {
-  response.send("Not Implemented: Site Home Page");
+  async.parallel(
+    {
+      book_count: function (callback) {
+        Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count: function (callback) {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: function (callback) {
+        BookInstance.countDocuments({ status: "Available" }, callback);
+      },
+      author_count: function (callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    function (error, results) {
+      response.render("index", {
+        title: "Local Library Home",
+        error: error,
+        data: results,
+      });
+    }
+  );
 };
 
 //__Display list of all Books__//
