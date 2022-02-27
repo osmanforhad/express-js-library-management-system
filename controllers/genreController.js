ba4e84b6bae552d43492b99cb55808b0f516f1cb
osmanforhad <osmanforhad93@gmail.com>
2022-02-27 18:48:01 +0600

    create genre details template page and modify the genre detail method inside the genre controller page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require("../models/genre");
+var Book = require("../models/book");
+var async = require("async");
 
 //__Display list of all Genres__//
 exports.genre_list = function (req, res, next) {
@@ -16,9 +18,36 @@ exports.genre_list = function (req, res, next) {
     });
 };
 
-//__Display Details of an specific Genre//
-exports.genre_detail = function (request, response) {
-  response.send("Not Implemented: Genre Details: " + request.params.id);
+//__Display detail page for a specific Genre__//
+exports.genre_detail = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        var err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 //__Display Genre create From on GET__//
