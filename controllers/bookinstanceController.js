f891bad12b19e15b302f15b928f78b394f086b94
osmanforhad <osmanforhad93@gmail.com>
2022-02-28 17:04:20 +0600

    create book instance form template and post user intput inot database

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 var BookInstance = require("../models/bookinstance");
+const { body, validationResult } = require("express-validator");
+var Book = require("../models/book");
 
 //__Display list of all BookInstances__//
 exports.bookinstance_list = function (req, res, next) {
@@ -39,14 +41,74 @@ exports.bookinstance_detail = function (req, res, next) {
 };
 
 //__Display BookInstance create From on GET__//
-exports.bookinstance_create_get = function (request, response) {
-  response.send("Not Implemented: BookInstance Create GET");
+exports.bookinstance_create_get = function (req, res, next) {
+  Book.find({}, "title").exec(function (err, books) {
+    if (err) {
+      return next(err);
+    }
+    // Successful, so render.
+    res.render("bookinstance_form", {
+      title: "Create BookInstance",
+      book_list: books,
+    });
+  });
 };
 
 //__Handle BookInstance create on POST__//
-exports.bookinstance_create_post = function (request, response) {
-  response.send("Not Implemented: BookInstance Create POST");
-};
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    var bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      Book.find({}, "title").exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+        // Successful, so render.
+        res.render("bookinstance_form", {
+          title: "Create BookInstance",
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance: bookinstance,
+        });
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      bookinstance.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new record.
+        res.redirect(bookinstance.url);
+      });
+    }
+  },
+];
 
 //__Display BookInstance Delete From on GET__//
 exports.bookinstance_delete_get = function (request, response) {
