373a7fa4ca3583bb43dbf529853ff3a70aa58e3a
osmanforhad <osmanforhad93@gmail.com>
2022-02-28 13:53:37 +0600

    create book entry template and post user input into database

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var BookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
-  summery: { type: String, required: true },
+  // summery: { type: String, required: true },
   isbn: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "Genre" },
 });
