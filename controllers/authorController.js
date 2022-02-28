cb3d6d2e7a7138a6aa30017e568909c8e9928498
osmanforhad <osmanforhad93@gmail.com>
2022-02-28 11:53:35 +0600

    create author form template and post user input into database

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 var Author = require("../models/author");
 var async = require("async");
 var Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 //__Display list of all Authors__//
 exports.author_list = function (req, res, next) {
@@ -49,15 +50,70 @@ exports.author_detail = function (req, res, next) {
   );
 };
 
-//__Display Author create From on GET__//
-exports.author_create_get = function (request, response) {
-  response.send("Not Implemented: Author Create GET");
+//__Display Author create form on GET__//
+exports.author_create_get = function (req, res, next) {
+  res.render("author_form", { title: "Create Author" });
 };
 
 //__Handle Author create on POST__//
-exports.author_create_post = function (request, response) {
-  response.send("Not Implemented: Author Create POST");
-};
+exports.author_create_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create an Author object with escaped and trimmed data.
+      var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new author record.
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 //__Display Author Delete From on GET__//
 exports.author_delete_get = function (request, response) {
